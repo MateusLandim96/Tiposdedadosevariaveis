2456a1810a8f5759e1f643d4b1b7b3d18989d3b5
MateusLandim96 <landim.b@gmail.com>
2021-12-09 14:53:02 -0300

    Tipos de dados e variáveis

---
+++ src/br/com/teste/Main.java
@@ -0,0 +1,65 @@
+package br.com.teste;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+        short s2 = 30000;
+
+        int i1 = -2500000;
+        int i2 =  28500;
+
+        long l1 = 10000000000000000L;
+        long l2 = 2004400500060005000L;
+        // Deve se colocar o "L" no final do valor do dado, para informar que se trata de uma variável long
+
+        float f1;
+        f1 = 4.5f;
+        float f2;
+        f2 = 10.60f;
+
+        double d1 = 85.98;
+        double d2 = 99.04d;
+            // ambas estão corretas, colocar o "d" no final do valor é opcional
+
+        char c1 = 'W';
+        // Usado para armazenar apenas um caractere, e deve se usado utilizando aspas simples.
+
+        String st1 = "Fulano";
+        String st2 = "Cicrano";
+        String st3 = "qualquer coisa";
+            //Para armazenar qualquer tipo de texto. Deve ser colocado utilizando aspas duplas
+
+        boolean bo1 = true;
+        boolean bo2 =  false;
+            //Tipo de dado para informar se a variavel é verdadeira ou falsa
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+
+    }
+
+
+
+
+
+
+
+}
